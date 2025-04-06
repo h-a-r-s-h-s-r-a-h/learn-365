1b2789e95130caf816fbc9dd528da7c0950c211b
Harsh <harshvirat894@gmail.com>
2025-04-07 01:38:57 +0530

    Day01

---
+++ tokenAndNft01/create-token-mint.ts
@@ -0,0 +1,16 @@
+import * as dotenv from "dotenv";
+import * as web3 from "@solana/web3.js";
+import {
+  getKeypairFromEnvironment,
+  getExplorerLink,
+} from "@solana-developers/helpers";
+
+dotenv.config();
+
+const connection = new web3.Connection(
+  web3.clusterApiUrl("devnet"),
+  "confirmed"
+);
+
+const user = await getKeypairFromEnvironment("SECRET_KEY");
+console.log(`Connection established with: `,user)
